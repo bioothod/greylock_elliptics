b3d239f6c8c383c6fa4eaba52fd2718ca4055650
Evgeniy Polyakov <zbr@ioremap.net>
2015-06-14 23:00:33 +0300

    server: added /index handler which requires ids/indexes arrays and updates given indexes with given ids

--- src/server.cpp
+++ src/server.cpp
@@ -1,5 +1,6 @@
 #include "indexes/bucket.hpp"
 #include "indexes/core.hpp"
+#include "indexes/index.hpp"
 
 #include <elliptics/session.hpp>
 
@@ -17,7 +18,7 @@
 #include <string>
 #include <thread>
 
-#define ILOG(verb, a...) BH_LOG(logger(), verb, ##a)
+#define ILOG(level, a...) BH_LOG(logger(), level, ##a)
 #define ILOG_ERROR(a...) ILOG(SWARM_LOG_ERROR, ##a)
 #define ILOG_WARNING(a...) ILOG(SWARM_LOG_WARNING, ##a)
 #define ILOG_INFO(a...) ILOG(SWARM_LOG_INFO, ##a)
@@ -101,6 +102,14 @@ public:
 		return indexes::bucket_processor::remove(key.bucket, key.key);
 	}
 
+	// this method is useless for bucket processing,
+	// each bucket will continue to work with all its groups,
+	// even if some of them are currently unavailable and
+	// index class reports this by setting new groups
+	void set_groups(const std::vector<int> &groups) {
+		(void) groups;
+	}
+
 private:
 };
 
@@ -136,12 +145,98 @@ public:
 		}
 	};
 
+	struct on_index : public thevoid::simple_request_stream<http_server> {
+		virtual void on_request(const thevoid::http_request &req, const boost::asio::const_buffer &buffer) {
+			(void) req;
+
+			// this is needed to put ending zero-byte, otherwise rapidjson parser will explode
+			std::string data(const_cast<char *>(boost::asio::buffer_cast<const char*>(buffer)), boost::asio::buffer_size(buffer));
+
+			rapidjson::Document doc;
+			doc.Parse<0>(data.c_str());
+
+			if (!doc.HasMember("ids")) {
+				ILOG_ERROR("url: %s, error: %d: there is no 'ids' member", req.url().to_human_readable().c_str(), -EINVAL);
+				this->send_reply(swarm::http_response::bad_request);
+				return;
+			}
+
+			if (!doc.HasMember("indexes")) {
+				ILOG_ERROR("url: %s, error: %d: there is no 'indexes' member", req.url().to_human_readable().c_str(), -EINVAL);
+				this->send_reply(swarm::http_response::bad_request);
+				return;
+			}
+
+
+			const auto &ids = doc["ids"];
+			const auto &idxs = doc["indexes"];
+
+			if (!ids.IsArray()) {
+				ILOG_ERROR("url: %s, error: %d: 'ids' must be array", req.url().to_human_readable().c_str(), -EINVAL);
+				this->send_reply(swarm::http_response::bad_request);
+				return;
+			}
+
+			if (!idxs.IsArray()) {
+				ILOG_ERROR("url: %s, error: %d: 'indexes' must be array", req.url().to_human_readable().c_str(), -EINVAL);
+				this->send_reply(swarm::http_response::bad_request);
+				return;
+			}
+
+
+			std::vector<std::string> raw_indexes;
+			std::vector<std::string> raw_ids;
+
+			for (auto it = idxs.Begin(), idx_end = idxs.End(); it != idx_end; ++it) {
+				if (it->IsString())
+					raw_indexes.push_back(it->GetString());
+			}
+
+			for (auto it = ids.Begin(), id_end = ids.End(); it != id_end; ++it) {
+				if (it->IsString())
+					raw_ids.push_back(it->GetString());
+			}
+
+
+			std::string bucket = "";
+
+			for (auto idx = raw_indexes.begin(), idx_end = raw_indexes.end(); idx != idx_end; ++idx) {
+				indexes::eurl start;
+				start.bucket = bucket;
+				start.key = *idx;
+
+				indexes::index<bucket_transport> index(*(server()->bucket()), start);
+
+				for (auto id = raw_ids.begin(), id_end = raw_ids.end(); id != id_end; ++id) {
+					indexes::key k;
+					k.url.bucket = bucket;
+					k.url.key = *id;
+					k.id = *id;
+
+					int err = index.insert(k);
+					if (err < 0) {
+						ILOG_ERROR("url: %s, index: %s, id: %s, error: %d: could not insert new key",
+							req.url().to_human_readable().c_str(), idx->c_str(), id->c_str(), err);
+						this->send_reply(swarm::http_response::internal_server_error);
+						return;
+					}
+				}
+			}
+
+			this->send_reply(thevoid::http_response::ok);
+		}
+	};
+
+	std::shared_ptr<bucket_transport> bucket() {
+		return m_bucket;
+	}
+
 private:
 	vector_lock m_lock;
 
 	std::shared_ptr<elliptics::node> m_node;
 
-	std::unique_ptr<bucket_transport> m_bucket;
+	std::shared_ptr<bucket_transport> m_bucket;
 
 	long m_read_timeout = 60;
 	long m_write_timeout = 60;
