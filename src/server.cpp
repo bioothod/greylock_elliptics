6d806cf0aecc8ebf50a64a4d2c3f131c798b9354
Evgeniy Polyakov <zbr@ioremap.net>
2015-06-17 03:28:10 +0300

    http: added /search method which intersects (using paginated output if requested) multiple indexes and sends json structure in reply

--- src/server.cpp
+++ src/server.cpp
@@ -2,14 +2,22 @@
 #include "indexes/bucket_transport.hpp"
 #include "indexes/core.hpp"
 #include "indexes/index.hpp"
+#include "indexes/intersection.hpp"
+
 
 #include <elliptics/session.hpp>
 
 #include <unistd.h>
 #include <signal.h>
+#include <time.h>
 
 #include <thevoid/server.hpp>
 #include <thevoid/stream.hpp>
+
+#include <thevoid/rapidjson/stringbuffer.h>
+#include <thevoid/rapidjson/prettywriter.h>
+#include <thevoid/rapidjson/document.h>
+
 
 #include <swarm/logger.hpp>
 
@@ -78,6 +86,72 @@ private:
 	std::vector<std::mutex> m_locks;
 };
 
+template <typename T>
+class locker {
+public:
+	locker(T *t, const std::string &key) : m_t(t), m_key(key) {}
+
+	void lock() {
+		m_t->lock(m_key);
+	}
+
+	void unlock() {
+		m_t->unlock(m_key);
+	}
+
+private:
+	T *m_t;
+	std::string m_key;
+};
+
+class JsonValue : public rapidjson::Value
+{
+public:
+	JsonValue() {
+		SetObject();
+	}
+
+	~JsonValue() {
+	}
+
+	static void set_time(rapidjson::Value &obj, rapidjson::Document::AllocatorType &alloc, long tsec, long usec) {
+		char str[64];
+		struct tm tm;
+
+		localtime_r((time_t *)&tsec, &tm);
+		strftime(str, sizeof(str), "%F %Z %R:%S", &tm);
+
+		char time_str[128];
+		snprintf(time_str, sizeof(time_str), "%s.%06lu", str, usec);
+
+		obj.SetObject();
+
+		rapidjson::Value tobj(time_str, strlen(time_str), alloc);
+		obj.AddMember("time", tobj, alloc);
+
+		std::string raw_time = elliptics::lexical_cast(tsec) + "." + elliptics::lexical_cast(usec);
+		rapidjson::Value tobj_raw(raw_time.c_str(), raw_time.size(), alloc);
+		obj.AddMember("time-raw", tobj_raw, alloc);
+	}
+
+	std::string ToString() const {
+		rapidjson::StringBuffer buffer;
+		rapidjson::PrettyWriter<rapidjson::StringBuffer> writer(buffer);
+
+		Accept(writer);
+		buffer.Put('\n');
+
+		return std::string(buffer.GetString(), buffer.Size());
+	}
+
+	rapidjson::MemoryPoolAllocator<> &GetAllocator() {
+		return m_allocator;
+	}
+
+private:
+	rapidjson::MemoryPoolAllocator<> m_allocator;
+};
+
 class http_server : public thevoid::server<http_server>
 {
 public:
@@ -107,6 +181,130 @@ public:
 			(void) req;
 
 			this->send_reply(thevoid::http_response::ok);
+		}
+	};
+
+	struct on_search : public thevoid::simple_request_stream<http_server> {
+		virtual void on_request(const thevoid::http_request &req, const boost::asio::const_buffer &buffer) {
+			(void) req;
+
+			// this is needed to put ending zero-byte, otherwise rapidjson parser will explode
+			std::string data(const_cast<char *>(boost::asio::buffer_cast<const char*>(buffer)), boost::asio::buffer_size(buffer));
+
+			rapidjson::Document doc;
+			doc.Parse<0>(data.c_str());
+
+			if (!doc.HasMember("indexes")) {
+				ILOG_ERROR("url: %s, error: %d: there is no 'indexes' member", req.url().to_human_readable().c_str(), -EINVAL);
+				this->send_reply(swarm::http_response::bad_request);
+				return;
+			}
+
+			const auto &idxs = doc["indexes"];
+
+			if (!idxs.IsArray()) {
+				ILOG_ERROR("url: %s, error: %d: 'indexes' must be array", req.url().to_human_readable().c_str(), -EINVAL);
+				this->send_reply(swarm::http_response::bad_request);
+				return;
+			}
+
+
+			size_t page_num = ~0U;
+			std::string page_start("\0");
+
+			if (doc.HasMember("paging")) {
+				const auto &pages = doc["paging"];
+				if (pages.HasMember("num")) {
+					const auto &n = pages["num"];
+					if (n.IsNumber())
+						page_num = n.GetUint64();
+				}
+
+				if (pages.HasMember("start")) {
+					const auto &s = pages["start"];
+					if (s.IsString())
+						page_start = s.GetString();
+				}
+			}
+
+			std::set<indexes::eurl> raw_indexes_set;
+			for (auto idx = idxs.Begin(), idx_end = idxs.End(); idx != idx_end; ++idx) {
+				if (!idx->IsString())
+					continue;
+
+				indexes::eurl start;
+				start.bucket = server()->meta_bucket_name();
+				start.key = idx->GetString();
+
+				raw_indexes_set.emplace(start);
+			}
+
+			std::vector<indexes::eurl> raw_indexes(raw_indexes_set.begin(), raw_indexes_set.end());
+
+			std::vector<indexes::key> result;
+			bool completed = true;
+
+			completed = intersect(raw_indexes, page_start, page_num, result);
+
+			JsonValue ret;
+			auto &allocator = ret.GetAllocator();
+
+			rapidjson::Value ids(rapidjson::kArrayType);
+			for (auto it = result.begin(), end = result.end(); it != end; ++it) {
+				rapidjson::Value key(rapidjson::kObjectType);
+
+				rapidjson::Value kv(it->url.key.c_str(), it->url.key.size(), allocator);
+				kv.AddMember("key", kv, allocator);
+
+				rapidjson::Value bv(it->url.bucket.c_str(), it->url.bucket.size(), allocator);
+				kv.AddMember("bucket", bv, allocator);
+
+				rapidjson::Value idv(it->id.c_str(), it->id.size(), allocator);
+				kv.AddMember("id", idv, allocator);
+
+				ids.PushBack(kv, allocator);
+			}
+
+			ret.AddMember("ids", ids, allocator);
+			ret.AddMember("completed", completed, allocator);
+
+			rapidjson::Value page(rapidjson::kObjectType);
+			page.AddMember("num", result.size(), allocator);
+
+			rapidjson::Value sv(page_start.c_str(), page_start.size(), allocator);
+			page.AddMember("start", sv, allocator);
+
+			ret.AddMember("paging", page, allocator);
+
+			data = ret.ToString();
+
+			thevoid::http_response reply;
+			reply.set_code(swarm::http_response::ok);
+			reply.headers().set_content_type("text/json; charset=utf-8");
+			reply.headers().set_content_length(data.size());
+
+			this->send_reply(std::move(reply), std::move(data));
+		}
+
+		bool intersect(const std::vector<indexes::eurl> &raw_indexes,
+				std::string &page_start, int page_num, std::vector<indexes::key> &result) {
+			indexes::intersect::intersector<indexes::bucket_transport> p(*(server()->bucket()));
+
+			std::vector<std::unique_lock<locker<http_server>>> locks;
+			for (auto it = raw_indexes.begin(), end = raw_indexes.end(); it != end; ++it) {
+				locker<http_server> l(server(), it->key);
+				std::unique_lock<locker<http_server>> lk(l);
+				locks.emplace_back(std::move(lk));
+			}
+
+			indexes::intersect::result res = p.intersect(raw_indexes, page_start, page_num);
+
+			if (res.keys.size()) {
+				auto &v = res.keys.begin()->second;
+				result.swap(v);
+			}
+
+			return res.completed;
 		}
 	};
 
@@ -184,27 +382,21 @@ public:
 				start.bucket = server()->meta_bucket_name();
 				start.key = idx->GetString();
 
-				server()->lock(start.key);
+				locker<http_server> l(server(), start.key);
+				std::unique_lock<locker<http_server>> lk(l);
 
-				try {
-					indexes::index<indexes::bucket_transport> index(*(server()->bucket()), start);
+				indexes::index<indexes::bucket_transport> index(*(server()->bucket()), start);
 
-					for (auto it = keys.begin(), end = keys.end(); it != end; ++it) {
-						int err = index.insert(*it);
-						if (err < 0) {
-							ILOG_ERROR("url: %s, index: %s, key: %s, error: %d: could not insert new key",
-								req.url().to_human_readable().c_str(), start.key.c_str(),
-								it->str().c_str(), err);
-							this->send_reply(swarm::http_response::internal_server_error);
-
-							server()->unlock(start.key);
-							return;
-						}
+				for (auto it = keys.begin(), end = keys.end(); it != end; ++it) {
+					int err = index.insert(*it);
+					if (err < 0) {
+						ILOG_ERROR("url: %s, index: %s, key: %s, error: %d: could not insert new key",
+							req.url().to_human_readable().c_str(), start.key.c_str(),
+							it->str().c_str(), err);
+						this->send_reply(swarm::http_response::internal_server_error);
+						return;
 					}
-				} catch (...) {
 				}
-
-				server()->unlock(start.key);
 			}
 
 			this->send_reply(thevoid::http_response::ok);
